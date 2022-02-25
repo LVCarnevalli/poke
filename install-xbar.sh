c591201071dcba873373ece193978487eb4b1365
Lucas Carnevalli <verdi@hotmail.com.br>
2022-02-25 13:29:19 -0300

    Add files via upload

--- install-xbar.sh
+++ install-xbar.sh
@@ -2,4 +2,6 @@
 
 cd ~/Library/Application\ Support/xbar/plugins && \
     curl https://raw.githubusercontent.com/LVCarnevalli/poke/main/poke.5m.py -O -J -L -s --output poke.5m.py && \
+    chmod 755 poke.5m.py && \
+    chmod +x poke.5m.py && \
     echo "OK"
